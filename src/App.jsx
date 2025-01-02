07678d7c309615312ce6dc5e95419685d0ea3f53
sandhyachan <sandhyachan6@gmail.com>
2025-01-02 15:48:31 +0530

    Modified pathways with direct rerendering for login token

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,44 @@
-import './App.css'
-import LoginPage from './pages/Login/Login'
-import RegisterPage from './pages/Registration/Registrations'
-import { Route, Routes } from 'react-router-dom'
-import UrlShortenerPage from './pages/UrlShortener/UrlShortener'
-import ActivationPage from './pages/Activation/Activation'
-import DashboardPage from './pages/Dashboard/Dashboard'
-
+import './App.css';
+import LoginPage from './pages/Login/Login';
+import RegisterPage from './pages/Registration/Registrations';
+import { Route, Routes } from 'react-router-dom';
+import ActivationPage from './pages/Activation/Activation';
+import DashboardPage from './pages/Dashboard/Dashboard';
+import { useEffect, useState } from 'react';
+import AllUrlsPage from './pages/AllUrls/AllUrls';
 
 function App() {
+  const [isLoggedIn, setLoggedIn] = useState(null)
+
+  useEffect(() => {
+    const token = localStorage.getItem('authToken')
+    if(token){
+      setLoggedIn(true)
+    } else {
+      setLoggedIn(false)
+    }
+  }, [])
+  if (isLoggedIn === null) {
+    return <div>Loading...</div>; // Or a spinner / loader
+  }
 
   return (
-    <>
     <Routes>
-      <Route Component={UrlShortenerPage} path='/shorten'/>
-      <Route Component={LoginPage} path='/'/>
-      <Route Component={RegisterPage} path='/registration'/>
-      <Route Component={ActivationPage} path='/activation'/>
-      <Route Component={DashboardPage} path='/dashboard'/>
+      {!isLoggedIn ? (
+        <>
+          <Route path="/" Component={()=><LoginPage setLoggedIn={setLoggedIn}/>} />
+          <Route path="/registration" Component={RegisterPage} />
+          <Route path="/activation" Component={ActivationPage} />
+        </>
+      ) : (
+        <>
+          <Route path="/dashboard" Component={()=><DashboardPage setLoggedIn={setLoggedIn}/>} />
+          <Route path="/all-urls" Component={AllUrlsPage} />
+          <Route path="*" /> 
+        </>
+      )}
     </Routes>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
